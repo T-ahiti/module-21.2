10afce18eb1d55271ae58221c16227c16a2d30e9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 16:41:28 +0600

    third commit

--- split.-join.js
+++ split.-join.js
@@ -4,5 +4,10 @@ const lyrics = 'tumi bondhu kala pakhi ami jeno ki. boshonto kale tomay dekhte p
 const parts = lyrics.split(' ')
 // console.log(parts)
 
+// . er age pore alada korse
 const part2 = lyrics.split('.')
 console.log(part2)
+
+// every letter alada korbe
+const part3 = lyrics.split('')
+console.log(part3)
