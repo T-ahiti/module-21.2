6d89241fc07c9efbed347d1dee7a0b3e32d4cc1e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 16:50:25 +0600

    fourth commit

--- split.-join.js
+++ split.-join.js
@@ -2,7 +2,7 @@ const lyrics = 'tumi bondhu kala pakhi ami jeno ki. boshonto kale tomay dekhte p
 
 // khali jayga gula jegula alada korse
 const parts = lyrics.split(' ')
-// console.log(parts)
+console.log(parts)
 
 // . er age pore alada korse
 const part2 = lyrics.split('.')
@@ -11,3 +11,16 @@ console.log(part2)
 // every letter alada korbe
 const part3 = lyrics.split('')
 console.log(part3)
+
+// fix kore dewa jay kothay shuru hiye koi sesh hobe, starting er index add hobe but ending er ta hobe na
+const partial = lyrics.slice(5,9)
+console.log(partial)
+
+const partial2 = lyrics.substring(5,9)
+console.log(partial2)
+
+
+const lines = [
+    'tumi bondhu kala pakhi ami jeno ki',
+    ' boshonto kale tomay dekhte parini'
+  ]
