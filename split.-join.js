acbf79441274f2fd0677807fefe1dab0905127d6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 16:39:42 +0600

    second commit

--- split.-join.js
+++ split.-join.js
@@ -1,1 +1,8 @@
-const lyrics = 'tumi bondhu kala pakhi ami jeno ki, boshonto kale tomay dekhte parini'
+const lyrics = 'tumi bondhu kala pakhi ami jeno ki. boshonto kale tomay dekhte parini'
+
+// khali jayga gula jegula alada korse
+const parts = lyrics.split(' ')
+// console.log(parts)
+
+const part2 = lyrics.split('.')
+console.log(part2)
