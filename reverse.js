42a4914c6136fa20cd2551c2ffb71954b7309ec7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 20:51:34 +0600

    3rd commit

--- reverse.js
+++ reverse.js
@@ -1,9 +1,13 @@
 function reverseString(text){
-    for(let i=0; i<text.length; i++){
+    let reversed = '';
+    for(let i=text.length-1; i>=0 ; i--){
         const element = text[i];
-        console.log(element)
+        reversed = reversed + element
+        console.log(element, reversed)
     }
+    return reversed;
 }
 
 const myString = 'i am a good girl'
 const reversed = reverseString(myString)
+console.log(reversed)
