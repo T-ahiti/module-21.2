705001629f8a47cc60e522138be8c11be3d02a4c
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 20:42:35 +0600

    second commit

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverseString(text){
+    for(let i=0; i<text.length; i++){
+        const element = text[i];
+        console.log(element)
+    }
+}
+
+const myString = 'i am a good girl'
+const reversed = reverseString(myString)
