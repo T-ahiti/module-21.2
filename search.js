1beeb8928b21126bc07d60e6546a9dcdf288af3b
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 15:16:34 +0600

    search added

---
+++ search.js
@@ -0,0 +1,7 @@
+const lyrics = 'tumi bondhu kala pakhi ami jeno ki, boshonto kale tomay dekhte parini'
+
+const doesExist = lyrics.includes('pakhi')
+console.log(doesExist)
+
+const doesExist2 = lyrics.includes('Pakhi')
+console.log(doesExist2)
