a54f419585d141999ec43c3419e68bd57aa4f4b9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 19:09:38 +0600

    second commit

---
+++ max.js
@@ -0,0 +1,12 @@
+
+const jim = 34;
+const dela = 23;
+const chinku = 42;
+
+if(jim > dela && jim > chinku){
+      console.log('jim cake pabe'); 
+}
+
+else if(dela > jim && dela > chinku){
+    console.log('dela cake pabe')
+}
