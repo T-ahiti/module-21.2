3f6ef5de377cde3bc7f44656225c59888b3fdd19
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 17:36:49 +0600

    second commit

--- math.js
+++ math.js
@@ -1,1 +1,18 @@
-const result = Math
+const result = Math.pow(3, 8)
+console.log(result)
+
+const num1 = 25
+const num2 = 45
+
+// const gap = num2 - num1, ETA dile problem hoto karon num1 choto. tai math use korle automatically oye  poromman ta accept kore just
+const gap = Math.abs(num2 - num1)
+if(gap < 5){
+    console.log('YOU GUYS CAN BE FRIENDS')
+}
+else{
+   console.log('YOU GUYS STAY APART')
+}
+
+const number = 2.56432523
+const fullNumber = Math.round(number)
+console.log(fullNumber)
