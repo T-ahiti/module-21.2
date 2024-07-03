d9f4bd984381f8fdd2e794629a3a1cabfc0283a6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 15:31:03 +0600

    second commit

--- search.js
+++ search.js
@@ -1,7 +1,23 @@
 const lyrics = 'tumi bondhu kala pakhi ami jeno ki, boshonto kale tomay dekhte parini'
 
 const doesExist = lyrics.includes('pakhi')
-console.log(doesExist)
+// console.log(doesExist)
 
 const doesExist2 = lyrics.includes('Pakhi')
-console.log(doesExist2)
+// console.log(doesExist2)
+
+const search = 'Pakhi'
+const doesExistOneLine = lyrics.toLowerCase().includes(search.toLowerCase())
+// console.log(doesExistOneLine)
+
+
+// ------------------------------------------------------
+// indexOf
+console.log(lyrics.indexOf('kala'))
+
+if( 'sada' !==-1){
+    console.log('doesExist')
+}
+else{
+    console.log('does not Exist')
+}
